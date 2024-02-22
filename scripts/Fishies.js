e002ea099cfaf2da5b503543514ec363b4f2f664
matt novotny <mattrnovotny@gmail.com>
2024-02-22 09:06:09 -0600

    its a good practice to commit

--- scripts/Fishies.js
+++ scripts/Fishies.js
@@ -65,8 +65,8 @@ export const FishList = () => {
             // Create HTML representations of each fish in the category
             for (const fish of fishes[category]) {
                 htmlString += `<section class="fish-card">
+                    <h3 class="fish__name fish__item">${fish.name}</h3>
                     <div><img class="fish__image image--card" src="${fish.image}" /></div>
-                    <div class="fish__name fish__item">name: ${fish.name}</div>
                     <div class="fish__species fish__item">species: ${fish.species}</div>
                     <div class="fish__length fish__item">size: ${fish.size}</div>
                     <div class="fish__location fish__item">harvest location:<a style="cursor: pointer;">${fish.harvest_location}</a></div>
