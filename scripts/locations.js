c871299cf94733d7b7ad3197cc50050527249f33
matt novotny <mattrnovotny@gmail.com>
2024-02-22 13:36:45 -0600

    message

--- scripts/locations.js
+++ scripts/locations.js
@@ -11,9 +11,9 @@ export const renderLocationList = () => {
     
     for (const location of locations) {
         htmlString += `<section class="location-card">
-            <div><img class="location__image" src="${location.image}" /></div>
-            <div class="location__name">${location.name}</div>
-            <div class="location__description">${location.description}</div>
+        <h3 class="location__name">${location.name}</h3>    
+        <div><img class="location__image" src="${location.image}" /></div>
+        <div class="location__description">${location.description}</div>
         </section>`;
     }
     
